b501381dffda4f8f7de4d8648b515225b0ad5a59
JORGE SOBRINHO <roteiro.ds@gmail.com>
2025-10-14 21:25:25 -0300

    Create worker.py

---
+++ worker.py
@@ -0,0 +1,18 @@
+# worker.py
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+from db import create_tables, heartbeat
+
+def beat():
+    heartbeat("worker", "alive")
+
+def main():
+    create_tables()
+    sch = BackgroundScheduler(timezone="UTC")
+    sch.add_job(beat, "interval", seconds=60, id="heartbeat", replace_existing=True)
+    sch.start()
+    while True:
+        time.sleep(3600)
+
+if __name__ == "__main__":
+    main()
