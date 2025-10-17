d99f762441ed1a4dda2ec1f1f3d2aea7163bae1d
JORGE SOBRINHO <roteiro.ds@gmail.com>
2025-10-17 15:03:27 -0300

    Update worker.py

--- worker.py
+++ worker.py
@@ -3,15 +3,14 @@ from datetime import datetime
 import os
 import pytz
 
-
 TZ = os.getenv("TZ", "America/Sao_Paulo")
 
 
 def main() -> None:
-tz = pytz.timezone(TZ)
-print(f"worker tick @ {datetime.now(tz).isoformat()}")
-# futuro: cálculos, atualização de PL%, geração de registros de EMAIL etc.
+    tz = pytz.timezone(TZ)
+    print(f"worker tick @ {datetime.now(tz).isoformat()}")
+    # futuro: cálculos, atualização de PL%, geração de registros de EMAIL etc.
 
 
 if __name__ == "__main__":
-main()
+    main()
