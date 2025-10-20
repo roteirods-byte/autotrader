d619284309a5cfe380c8656cd3234921b96654f6
JORGE SOBRINHO <roteiro.ds@gmail.com>
2025-10-20 11:26:45 -0300

    Update aplicativo.py

--- aplicativo.py
+++ aplicativo.py
@@ -1,20 +1,25 @@
 import streamlit as st
 from ui import theme
 from panels.email_panel import render_email_panel
-from panels.moedas_panel import render_moedas_panel
+from panels.operador_panel import render_operador_panel  # novo
 
 def main():
     st.set_page_config(page_title="Autotrader", page_icon="📈", layout="centered")
     theme.inject_streamlit(st)
 
     st.markdown("## PAINÉIS DA AUTOMAÇÃO")
-    tab_email, tab_moedas, tab_entrada, tab_saida = st.tabs(["EMAIL", "MOEDAS", "ENTRADA", "SAÍDA"])
+    tab_operador, tab_email, tab_moedas, tab_entrada, tab_saida = st.tabs(
+        ["OPERADOR", "EMAIL", "MOEDAS", "ENTRADA", "SAÍDA"]
+    )
+
+    with tab_operador:
+        render_operador_panel()
 
     with tab_email:
         render_email_panel()
 
     with tab_moedas:
-        render_moedas_panel()
+        st.info("Painel MOEDAS em preparação.")
 
     with tab_entrada:
         st.info("Painel ENTRADA em preparação.")
